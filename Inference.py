c09810ee97a0b60d2a91038f0a03f75f6ce387fd
LeanMamoru <lean.mamoru@gmail.com>
2022-03-19 11:55:28 +1000

    move mouse and double click

--- Inference.py
+++ Inference.py
@@ -6,4 +6,6 @@ print(f"screenWidth: {screenWidth}, screenHeight: {screenHeight}")
 currentMouseX, currentMouseY = pyautogui.position()
 print(f"currentMouseX: {currentMouseX}, currentMouseY: {currentMouseY}")
 
-pyautogui.moveTo(100, 150)  # https://pyautogui.readthedocs.io/en/latest/
+pyautogui.move(-400, 100)
+pyautogui.doubleClick()  # https://pyautogui.readthedocs.io/en/latest/
+
