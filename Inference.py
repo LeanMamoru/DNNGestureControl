2d130da62d6daa2e4a7a15a12b62ad66cf8abb05
LeanMamoru <lean.mamoru@gmail.com>
2022-03-19 11:44:08 +1000

    1) added mouse control 2) added resource with docs as commented link

--- Inference.py
+++ Inference.py
@@ -6,4 +6,4 @@ print(f"screenWidth: {screenWidth}, screenHeight: {screenHeight}")
 currentMouseX, currentMouseY = pyautogui.position()
 print(f"currentMouseX: {currentMouseX}, currentMouseY: {currentMouseY}")
 
-
+pyautogui.moveTo(100, 150)  # https://pyautogui.readthedocs.io/en/latest/
