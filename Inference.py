e7e2ae10a1856f4120cba8ae946f5aec6c3aac0d
LeanMamoru <lean.mamoru@gmail.com>
2022-03-26 14:03:09 +1000

    1) Adding camera capture 2) Created preprocessing.py 3) Adding mediaPipe hand_extractor function in preprocessing.py. For getting hand and hand_points images and hand_pose (x, y) 4) Created mouse_control.py 5) Added function for move mouthe by hands

--- Inference.py
+++ Inference.py
@@ -1,11 +1,35 @@
-import pyautogui
+import cv2
+from preprocessing import hand_extractor
+from mouse_control import move_mouse
 
-screenWidth, screenHeight = pyautogui.size()
-print(f"screenWidth: {screenWidth}, screenHeight: {screenHeight}")
 
-currentMouseX, currentMouseY = pyautogui.position()
-print(f"currentMouseX: {currentMouseX}, currentMouseY: {currentMouseY}")
+def start_recognition():
+    print("Starting camera ...")
+    camera = cv2.VideoCapture(0)
+    width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)  # float `width`
+    height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-pyautogui.move(-400, 100)
-pyautogui.doubleClick()  # https://pyautogui.readthedocs.io/en/latest/
+    while (True):
+
+        _, frame = camera.read()
+
+        hand_points_image, hand_image, x, y = hand_extractor(frame, width, height)
+        if x >= 0 and y >= 0:
+            move_mouse(x, y)
+
+        cv2.imshow('Hand skeleton', hand_points_image)
+
+        cv2.imshow('Hand', hand_image)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    camera.release()
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    start_recognition()
+
 
